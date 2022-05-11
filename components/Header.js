1b95a3a26091e9db5944f3de096248d3e87d456b
KatjavanWeert <44201968+KatjavanWeert@users.noreply.github.com>
2022-05-11 15:55:30 +0200

    Add Tailwind

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,3 @@
 export default function Header({ title, subtitle }) { //title = properties
-  return <h1 className="title">{title}{subtitle}</h1>
+  return <h1 className="text-3xl font-bold underline">{title}{subtitle}</h1>
 }
