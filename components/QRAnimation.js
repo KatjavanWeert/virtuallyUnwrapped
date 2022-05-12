3ca55e884f07b75a1ada5231fc794ac0a9ac99f5
KatjavanWeert <44201968+KatjavanWeert@users.noreply.github.com>
2022-05-12 18:49:22 +0200

    AddQR

---
+++ components/QRAnimation.js
@@ -0,0 +1,6 @@
+export default function QRAnimation() {
+    return <div>
+<img src="/QR.png" className="object-center object-cover h-full w-full p-5"/>
+    </div>
+  }
+  
--- pages/giver.js
+++ pages/giver.js
@@ -5,6 +5,7 @@ import { faPlusCircle } from '@fortawesome/free-solid-svg-icons'
 import IconButton from '@components/IconButton'
 import PhotoFrame from '@components/PhotoFrame'
 import MediaButtonBar from '@components/MediaButtonBar'
+import QRAnimation from '@components/QRAnimation'
 
 export default function GiverPage() {
     function logToConsole(text) {
@@ -14,17 +15,19 @@ export default function GiverPage() {
     return (
         <div className="relative h-screen w-full">
             <img src="/bg1.png" className="absolute object-bottom object-cover h-full w-full" />
+            <div className='absolute bottom-44 w-full flex flex-col items-center space-y-6'>
+                <IconButton icon={faPlusCircle} />
+                <QRAnimation />
+            </div>
             <div className="absolute w-full">
-
-                <div className='flex flex-col w-full items-center space-y-6'>
+                <div className='flex flex-col w-full items-center'>
                     <div className="px-2 pt-4 flex flex-row">
                         <Header icon={faEarthAmericas} title={"PUBLIC SPACE"} subtitle={"For every future gift recipient"} />
                     </div>
-                    <IconButton icon={faPlusCircle} />
-                    <PhotoFrame photo={'bg2.png'} />
-                    <MediaButtonBar />
-                    {/* <IconButtonSquare onClick={() => logToConsole("hey Frankyy")} icon={faImage}/> */}
-
+                    <div className="pt-36 space-y-2">
+                        <PhotoFrame photo={'bg2.png'} />
+                        <MediaButtonBar />
+                    </div>
                 </div>
             </div>
         </div>
