a90fa4840ab1cf309e3138e495650093101e1735
KatjavanWeert <44201968+KatjavanWeert@users.noreply.github.com>
2022-05-14 21:11:07 +0200

    Reset and save inputs

--- pages/giver.js
+++ pages/giver.js
@@ -6,7 +6,7 @@ import IconButton from "@components/IconButton";
 import PhotoFrame from "@components/PhotoFrame";
 import MediaButtonBar from "@components/MediaButtonBar";
 import QRAnimation from "@components/QRAnimation";
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { faCircleCheck } from "@fortawesome/free-solid-svg-icons";
 import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 
@@ -26,11 +26,33 @@ export default function GiverPage() {
     console.log("upload text");
   }
 
+  function saveCurrentMedia(currentMediaType) {
+    switch (currentMediaType) {
+      case "Text":
+        console.log(textUpload);
+        return;
+      case "Image":
+        console.log(imageUpload);
+        return;
+    }
+  }
+  function resetAllMedia() {
+    setTextUpload("");
+    setSelectedImage("/bg2.png");
+    setImageUpload(null);
+  }
+
   const [selectedImage, setSelectedImage] = useState("/bg2.png");
   const [showMediaPanel, setShowMediaPanel] = useState(false);
+  const [mediaType, setMediaType] = useState(null);
   const [opacityQR, setOpacityQR] = useState("opacity-100");
-  const [textUpload, setTextUpload] = useState("Write your message here");
+  const [textUpload, setTextUpload] = useState("");
+  const [imageUpload, setImageUpload] = useState(null);
   const inputRef = useRef(null);
+
+  useEffect(() => {
+    console.log(mediaType);
+  }, [mediaType]);
 
   return (
     <div className="relative h-screen w-full ">
@@ -70,28 +92,40 @@ export default function GiverPage() {
           {showMediaPanel == true && (
             <div className="pt-26 space-y-2">
               <MediaButtonBar
-                onUploadImage={() => inputRef.current.click()}
-                onUploadText={() => uploadText()}
+                onUploadImage={() => {
+                  setMediaType("Image");
+                  inputRef.current.click();
+                }}
+                onUploadText={() => setMediaType("Text")}
               />
               <PhotoFrame photo={selectedImage}>
-                <textarea
-                  className="absolute p-10 bg-transparent text-center w-80 h-80 font-bold text-qrBlue text-lg"
-                  value={textUpload}
-                  onChange={(event) => {
-                    setTextUpload(event.target.value);
-                    console.log(textUpload);
-                  }}
-                />
+                {mediaType == "Text" && (
+                  <textarea
+                    className="absolute p-10 bg-transparent text-center w-80 h-80 font-bold text-qrBlue text-lg"
+                    value={textUpload}
+                    placeholder="Write your message here!"
+                    onChange={(event) => {
+                      setTextUpload(event.target.value);
+                    }}
+                  />
+                )}
               </PhotoFrame>
 
               <div className="flex flex-row w-full space-x-48 justify-center">
                 <IconButton
                   icon={faTrashCan}
-                  onClick={() => toggleMediaUpload()}
+                  onClick={() => {
+                    toggleMediaUpload();
+                    resetAllMedia();
+                  }}
                 />
                 <IconButton
                   icon={faCircleCheck}
-                  onClick={() => toggleMediaUpload()}
+                  onClick={() => {
+                    toggleMediaUpload();
+                    saveCurrentMedia(mediaType);
+                    resetAllMedia();
+                  }}
                 />
               </div>
             </div>
@@ -107,7 +141,7 @@ export default function GiverPage() {
         ref={inputRef}
         accept="image/*"
         onChange={(event) => {
-          console.log(event.target.files[0]);
+          setImageUpload(event.target.files[0]);
           const objectURL = URL.createObjectURL(event.target.files[0]);
           setSelectedImage(objectURL);
         }}
