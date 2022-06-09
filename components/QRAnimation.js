1deecf55a85e21fcf8422322bffd1b037deec205
KatjavanWeert <44201968+KatjavanWeert@users.noreply.github.com>
2022-06-09 11:34:53 +0200

    add videotype

--- components/QRAnimation.js
+++ components/QRAnimation.js
@@ -2,10 +2,11 @@ export default function QRAnimation() {
   return (
     <div>
       <video
-        // autoPlay
+        autoPlay
         loop
         muted
         playsInline
+        type="video/mp4"
         src="/FloatingQR.mp4"
         className="absolute object-bottom object-cover h-full w-full"
       />
